f4a90445ce1fbef9c282a200e89f7427d9c12df3
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-09-24 20:25:05 +0400

    Spring properties file as a map

--- src/main/java/com/arpit/config/ApplicationConfig.java
+++ src/main/java/com/arpit/config/ApplicationConfig.java
@@ -1,9 +1,12 @@
 package com.arpit.config;
 
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @PropertySources({
@@ -12,4 +15,10 @@ import org.springframework.context.annotation.PropertySources;
 @ComponentScan(basePackages = "com.arpit.controller")
 public class ApplicationConfig {
 
+	@Bean(name = "keys")
+	public static PropertiesFactoryBean keys() {
+		PropertiesFactoryBean bean = new PropertiesFactoryBean();
+		bean.setLocation(new ClassPathResource("key.properties"));
+		return bean;
+	}
 }
--- src/main/java/com/arpit/controller/HelloController.java
+++ src/main/java/com/arpit/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.arpit.controller;
 
+import java.util.Map;
+
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+	@Resource(name = "keys")
+	private Map<String, String> keys;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
